4e347d8057d004b64a06ef2137e1bff9222a4d08
kleiduda <kleiduda@gmail.com>
2025-01-05 11:31:41 -0300

    feat(Http): 1 - adiciona modulo base de requisicoes HTTP; 2 - inclui arquivo de config pra urls dos modulos; 3- inclui requisicao para tela de ferramentas

---
+++ MechanicalOs-Angular/src/app/Http/models/Input/get-all-request.model.ts
@@ -0,0 +1,7 @@
+export interface GetAllRequest {
+    pageSize: number;
+    pageIndex: number;
+    sort?: string;
+    direction?: string;
+  }
+  
--- MechanicalOs-Angular/src/app/apps/tools/tools.component.ts
+++ MechanicalOs-Angular/src/app/apps/tools/tools.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { BreadcrumbItem } from "src/app/shared/page-title/page-title.model";
 import { TOOLS } from "./data";
 import { ToolsModel } from "./tools.model";
+import { ToolsService } from "./tools.service";
+import { GetAllRequest } from "src/app/Http/models/Input/get-all-request.model";
 
 @Component({
   selector: "app-tools",
@@ -12,10 +14,29 @@ export class ToolsComponent implements OnInit {
   pageTitle: BreadcrumbItem[] = [];
   tools: ToolsModel[] = [];
 
-  constructor() {}
+  constructor(private service: ToolsService) {}
 
   ngOnInit(): void {
     this.pageTitle = [{ label: "Home", path: "/", active: true }];
     this.tools = TOOLS;
+    //this.loadToolsCard();
+  }
+
+  loadToolsCard(): void{
+    const request: GetAllRequest = {
+      pageSize: 10,
+      pageIndex: 1,
+      sort: '',
+      direction: ''
+    };
+    
+    this.service.getModules().subscribe((result) =>{
+      if(result.statusCode == 200){
+        console.log(result);
+        this.tools = result.content.resultList;
+      }else{
+        console.error(result.message);
+      }
+    })
   }
 }
